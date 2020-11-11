e0f28110bfdd5ee840fd8ea098856c514606efaa
EC2 Default User <ec2-user@ip-172-31-23-201.ec2.internal>
2020-11-11 10:49:21 +0000

    [add]２章

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # devise_parameter_sanitizer.permitでnameのデータ操作を許可するアクションメソッドが指定されている
+    # 今回のケースでは、ユーザ登録（sign_up）の際に、ユーザ名（name）のデータ操作が許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+end
